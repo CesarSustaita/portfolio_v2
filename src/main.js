8f89f2f648681b85474ea88e2c671efecb830f4f
Cesar Sustaita <c.sustaita@outlook.com>
2025-08-04 22:01:00 -0600

    Add: Vercel Analytics and Speed Insights

--- src/main.js
+++ src/main.js
@@ -6,6 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
+inject();
+injectSpeedInsights();
+
 const app = createApp(App)
 
 app.use(createPinia())
